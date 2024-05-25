1f93983af192162fe8c75ac417e66c08555d3d95
fjx <1300448152@qq.com>
2024-05-25 23:20:11 +0800

    style(login-header):修改登录页面中header 的部分样式

--- src/views/login-register/cpns/header.vue
+++ src/views/login-register/cpns/header.vue
@@ -4,7 +4,7 @@
     <img class="w-full" src="https://res.lgdsunday.club/signlogo.png" alt="">
   </div>
   <!-- 移动端图标 -->
-  <div class="w-full h-[100px] block xl:hidden relative ">
+  <div class="w-full h-[100px] relative  xl:hidden  ">
     <img src="https://res.lgdsunday.club/login-bg.png" class="dark:hidden">
     <img class=" absolute top-[40%] left-[50%] translate-x-[-50%] w-6"
       src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png" alt="">
