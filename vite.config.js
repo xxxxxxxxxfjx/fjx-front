51d2a35ff76b3ecf2aaefe1774cf5b2bed0564db
fjx <1300448152@qq.com>
2024-06-12 14:27:14 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -26,9 +26,9 @@ export default defineConfig({
       }
     }
   },
-  build: {
-    sourcemap: true
-  },
+  // build: {
+  //   sourcemap: true
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
